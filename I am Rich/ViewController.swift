f50056a52b49bd1dae432ea0ca145287ea55e71e
Jean Carlos Izepon <izeponjc@gmail.com>
2022-07-18 20:28:11 -0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Jean Carlos Izepon on 14/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
